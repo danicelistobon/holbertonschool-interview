52edd6788861a77cb27d2a21934ee7ccd7a5158f
Daniel <danielct-97@hotmail.com>
2021-05-20 11:31:20 -0500

    0. Infinite Multiplication

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,95 @@
+#include "holberton.h"
+/**
+* main - multiplies two numbers
+* @argc: number of things in the command line
+* @argv: array of string values
+* Return: 0 in success
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	multiply(argv[1], argv[2]);
+	return (0);
+}
+
+/**
+* _isdigit - Checks for a digit (0 through 9)
+* @c: digit
+* Return: return 1 if c is a digit or 0 otherwise
+*/
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+* multiply - multiplies two numbers
+* @a: first num
+* @b: second num
+*/
+void multiply(char *a, char *b)
+{
+	int i, len_a, len_b, total, a_number, b_number, res = 0, tmp;
+	int *ptr;
+
+	len_a = _strlen(a);
+	len_b = _strlen(b);
+	tmp = len_b;
+	total = len_a + len_b;
+	ptr = malloc(sizeof(int) * total);
+	if (!ptr)
+	{
+		return;
+	}
+	for (len_a--; len_a >= 0; len_a--)
+	{
+		a_number = a[len_a] - '0';
+		res = 0;
+		len_b = tmp;
+		for (len_b--; len_b >= 0; len_b--)
+		{
+			b_number = b[len_b] - '0';
+			res += ptr[len_b + len_a + 1] + (a_number * b_number);
+			ptr[len_a + len_b + 1] = res % 10;
+			res /= 10;
+		}
+		if (res)
+			ptr[len_a + len_b + 1] = res % 10;
+	}
+	while (*ptr == 0)
+	{
+		ptr++;
+		total--;
+	}
+	for (i = 0; i < total; i++)
+		printf("%i", ptr[i]);
+	printf("\n");
+}
+
+/**
+* _strlen - returns the length of a string
+* @s: pointer
+* Return: length
+*/
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int _isdigit(int c);
+void multiply(char *a, char *b);
+int _strlen(char *s);
+
+#endif
