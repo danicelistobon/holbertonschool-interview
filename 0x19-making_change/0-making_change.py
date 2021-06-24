a38f8bc52206c1bae45f43977354db10aecbc84f
Daniel <danielct-97@hotmail.com>
2021-06-24 22:20:11 +0000

    0. Change comes from within

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Change comes from within
+"""
+
+
+def makeChange(coins, total):
+    """ Given a pile of coins of different values, determine the fewest number
+        of coins needed to meet a given amount total
+    """
+    if total < 1:
+        return 0
+
+    array = [float('inf')] * (total + 1)
+    array[0] = 0
+
+    for i in range(1, len(array)):
+        for j in range(len(coins)):
+            if coins[j] <= i:
+                array[i] = min(array[i], array[i - coins[j]] + 1)
+
+    return array[i] if array[i] != float('inf') else -1
